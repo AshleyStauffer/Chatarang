2b26c405b180c8f02d97635d162d3d9bf43831fa
Ashley Stauffer <anstauffer21@gmail.com>
2018-07-10 10:25:05 -0400

    Add "new room" button to sidebar

--- src/Main.js
+++ src/Main.js
@@ -6,80 +6,82 @@ import RoomForm from './RoomForm'
 import base from './base'
 
 class Main extends Component {
-    state = {
-        room: {
+  state = {
+    room: {
+      name: 'general',
+      description: 'Chat about stuff',
+    },
+    rooms: {},
+    showRoomForm: false,
+  }
+
+  componentDidMount() {
+    this.roomsRef = base.syncState(
+      'rooms',
+      {
+        context: this,
+        state: 'rooms',
+        defaultValue: {
+          general: {
             name: 'general',
             description: 'Chat about stuff',
-        },
-        rooms: {},
-        showRoomForm: false,
+          },
+        }
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.roomsRef)
+  }
+
+  addRoom = room => {
+    const rooms = {...this.state.rooms}
+    rooms[room.name] = room
+
+    this.setState({ rooms })
+  }
+
+  setCurrentRoom = roomName => {
+    const room = this.state.rooms[roomName]
+    this.setState({ room })
+  }
+
+  showRoomForm = () => {
+    this.setState({ showRoomForm: true })
+  }
+
+  hideRoomForm = () => {
+    this.setState({ showRoomForm: false })
+  }
+
+  render() {
+    if (this.state.showRoomForm) {
+      return <RoomForm addRoom={this.addRoom} />
     }
 
-    componentDidMount() {
-        this.roomsRef = base.syncState(
-            'rooms',
-            {
-                context: this,
-                state: 'rooms',
-                defaultValue: {
-                    general: {
-                        name: 'general',
-                        description: 'Chat about stuff',
-                    },
-                }
-            }
-        )
-    }
-
-    componentWillUnmount() {
-        base.removeBinding(this.roomsRef)
-    }
-
-    addRoom = room => {
-        const rooms = {...this.state.rooms}
-        rooms[room.name] = room 
-        this.setState({ rooms })
-    }
-    setCurrentRoom = roomName => {
-        const room = this.state.rooms[roomName]
-        this.setState({ room })
-    }
-
-    showRoomForm = () => {
-        this.setState({ showRoomForm: true })
-    }
-
-    hideRoomForm = () => {
-        this.setState({showRoomForm: false })
-    }
-    render() {
-        return (
-            <div className="Main" style={styles}>
-                {
-                    this.state.showRoomForm
-                        ? <RoomForm addRoom={this.addRoom} />
-                        : <Fragment>
-                            <Sidebar 
-                                user={this.props.user}
-                                signOut={this.props.signOut}
-                                rooms={this.state.rooms}
-                                setCurrentRoom={this.setCurrentRoom}
-                            />
-                            <Chat 
-                                user={this.props.user}
-                                room={this.state.room}
-                            />
-                        </Fragment>
-                }
-            </div>
-        )
-    }
+    return (
+      <div className="Main" style={styles}>
+        <Sidebar
+          user={this.props.user}
+          signOut={this.props.signOut}
+          rooms={this.state.rooms}
+          setCurrentRoom={this.setCurrentRoom}
+          showRoomForm={this.showRoomForm}
+        />
+        <Chat
+          user={this.props.user}
+          room={this.state.room}
+        />
+      </div>
+    )
+  }
 }
 
 const styles = {
-    display: 'flex',
-    alignItems: 'stretch',
-    height: '100vh',
+  display: 'flex',
+  alignItems: 'stretch',
+  height: '100vh',
 }
 
 export default Main
